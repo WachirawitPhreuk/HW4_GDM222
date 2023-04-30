e90cc0b575a42da87551164771b04302767997c4
WachirawitPhreuk <wachirawit.pruk@mail.kmutt.ac.th>
2023-04-30 19:20:43 +0700

    Q4
    Question 4

--- Q4/Program.cs
+++ Q4/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+    static void Main(string[] args) {
+    
+    int n = int.Parse(Console.ReadLine()); //จำนวนโหนดในกราฟที่ไม่มีทิศทาง
+    
+    List<List<int>> g = new List<List<int>>();
+    for(int i=0; i<n; i++) {
+        List<int> temp = new List<int>();
+        g.Add(temp);
+    }
+
+    while(true) {
+        int i = int.Parse(Console.ReadLine());
+        if(i>=n) {
+            break;
+        }
+        else if(i<0) {
+            break;
+        }
+        
+        int j = int.Parse(Console.ReadLine());
+        if(j>=n) {
+            break;
+        }
+        else if(j<0) {
+            break;
+        }
+
+        if(i==j) {
+            break;
+        }
+
+        g[i].Add(j);
+        g[j].Add(i);
+    }
+
+
+    int[] color = new int[n];
+
+    for(int i=0; i<n; i++) {
+        foreach (int edge in g[i]) {
+            if(color[i] == color[edge]) {
+                color[edge] += 1;
+            }
+        }
+    }
+
+    int max = -100;
+    for(int i=0; i<n; i++) {
+        if(color[i] >= max) {
+            max = color[i];
+        }
+    }
+
+    Console.WriteLine(max+1);
+    }
+}
